6d30b00ef7997a4aa50a1b1b6ba7f3f439156116
huyQuang <huyhuy8122001@gmai.com>
2022-01-22 18:18:02 +0700

    C2 main files commit

---
+++ Data/app copy.py
@@ -0,0 +1,132 @@
+import dash
+#import dash_html_components as html
+#import dash_core_components as dcc
+from dash import html
+from dash import dcc
+from dash_extensions import Download
+from dash_extensions.snippets import send_data_frame
+from dash.dependencies import Input, Output
+# from dash import Dash, dcc, html, Input, Output
+
+import pandas as pd
+import plotly.express as px
+
+#Dash app
+app = dash.Dash(__name__)
+
+#data
+df = pd.read_csv('my_backup.csv') #data frame
+df['Date'] = pd.to_datetime(df['Date'])
+
+#My Graph
+fig = px.line(df, x='Date', y= 'Hosts', color='Status', title='Backup status', height=550, 
+            color_discrete_map={'successful':'#27ae60','failed':'#e55039'},
+            line_shape='linear', template='presentation') #markers=True
+
+fig.update_traces(line=dict(width=2.5))
+
+fig.update_xaxes(
+    rangeselector=dict(
+        buttons=list([
+            dict(step="all"),
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=7, label="1w", step="day", stepmode="backward"),
+            dict(count=1, label="24h", step="day", stepmode="backward"),
+            dict(count=1, label="today", step="day", stepmode="todate")
+        ])
+    ),
+    #type= 'date'
+    #autorange= False,
+    rangeslider=dict(
+            visible=True
+    ),
+)
+
+colors = {
+    'background': '#111111',
+    'text': '#8cc1fa',
+    'sub_text':'#1b5ea6'
+}
+
+#Layout
+app.layout = html.Div(children=[
+    html.H1(children='Dashboard',
+            style={
+            'textAlign': 'center',
+            'color': colors['text'],
+            'fontSize': 50
+            }
+    ),
+
+    html.H3(children='Describe devices backup status.', style={
+        'textAlign': 'center',
+        'color': colors['sub_text'],
+        'fontSize': 20
+    }),
+
+    html.Div(
+        [
+        html.Br(),
+        dcc.Graph(id='line-chart',
+            figure=fig),
+        
+        html.Button("Download csv", id="btn"), 
+        Download(id="download")
+        ]
+    )
+    
+])
+
+@app.callback(
+    Output("download", "data"), 
+    [Input("btn", "n_clicks")]
+)
+
+def generate_csv(n_nlicks):
+    return send_data_frame(df.to_csv, filename="BACKUP_INFO.csv")
+
+# #Callback
+# @app.callback(
+#     Output(component_id='price-graph', component_property='figure'),
+#     [Input(component_id='geo-dropdown', component_property='value')]
+# )
+# def update_graph(selected_geography):
+#     filtered_avocado = avocado[avocado['geography'] == selected_geography]
+#     line_fig = px.line(filtered_avocado,
+#                        x='date', y='average_price',
+#                        color='type',
+#                        title=f'Avocado Prices in {selected_geography}')
+#     return line_fig
+
+
+# Run app
+if __name__ == '__main__':
+    app.run_server(port=8151, debug=True, use_reloader=False)
+
+'''
+
+import dash
+import dash_html_components as html
+import numpy as np
+import pandas as pd
+
+from dash.dependencies import Output, Input
+from dash_extensions import Download
+from dash_extensions.snippets import send_data_frame
+
+# Generate some example data.
+data = np.column_stack((np.arange(10), np.arange(10) * 2))
+df = pd.DataFrame(columns=["a column", "another column"], data=data)
+# Create app.
+app = dash.Dash(prevent_initial_callbacks=True)
+app.layout = html.Div([html.Button("Download csv", id="btn"), Download(id="download")])
+
+@app.callback(Output("download", "data"), [Input("btn", "n_clicks")])
+def generate_csv(n_nlicks):
+    return send_data_frame(df.to_csv, filename="some_name.csv")
+
+if __name__ == '__main__':
+    app.run_server()
+
+
+'''
---
+++ Data/get_data.py
@@ -0,0 +1,82 @@
+
+from netmiko import ConnectHandler
+from datetime import datetime
+import csv
+
+
+now = str(datetime.now().strftime("%Y-%m-%d %H:%M")) #%Y-%m-%d, %H:%M:%S
+
+switch_num = 1 # Count node number
+
+success = 0  
+fail = 0
+
+with open('my_backup.csv', 'a', newline='') as f:
+    write = csv.writer(f)
+    #write.writerow(['Total', 'Status', 'Hosts', 'Date'])
+
+    with open('ip.txt') as switches:
+        for ip in switches:
+            print('Backing up configuration SW'+ str(switch_num) + ' ' + ip)
+            print('.....')
+            print(' ')
+            SW = {
+                'device_type':'cisco_ios',
+                'ip':ip,
+                'username':'admin',
+                'password':'vnpro',
+                'secret':'cisco'
+            }
+            try:
+                ssh = ConnectHandler(**SW)      
+
+            except:
+                print('~ Backup failed !!! , check error in file backup_error.log ~')
+                print('\n')
+                
+                # write in log file
+                with open("backup_error.log", 'a') as logf:
+                    logf.write('Backup Failed on ')
+                    logf.write(now)
+                    logf.write(" - Cannot connect via SSH or Telnet to SW")
+                    logf.write(str(switch_num))
+                    logf.write(' ')
+                    logf.write(ip)
+                    logf.write('\n')
+                sw_name = 'SW' + str(switch_num)
+                ###write.writerow([sw_name, switch_num, ip, 'failed', now])
+
+                fail += 1
+                switch_num += 1 
+                
+                continue
+            
+            ssh.enable()
+            position = ssh.find_prompt() #=> SW1 
+            # backup file
+            sw_name=position[:-1]   #  -> SW1     
+            filename = sw_name + '.txt'
+            
+            show_run = ssh.send_command('show running-config')   
+            
+            with open(f"backupFiles/{filename}", "w") as log_file:
+                log_file.write("#"*26)
+                log_file.write(' DATE: '+ now + ' ')
+                log_file.write("#"*26 + '\n')
+                log_file.write(show_run)
+            print('~ Successful !! ~')
+            print('\n')
+            # write
+            ###write.writerow([sw_name, switch_num, ip, 'successful', now])
+            switch_num += 1
+            success += 1
+        
+        #write.writerow(['Hosts', 'Status','Count' 'Date'])
+        write.writerow([switch_num - 1, 'successful', success, now])
+        write.writerow([switch_num - 1, 'failed', fail, now])
+
+        print('*'*35+' DONE!!! '+'*'*35)
+        print("-> Successful:",success)
+        print("-> Failed:",fail)
+        
+
